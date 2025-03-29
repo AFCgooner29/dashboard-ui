5414f2fba46380fbd462a901bd8f12f1f10732c8
AFCgooner29 <sharanmeetsekhon@gmail.com>
2025-03-29 13:42:05 +0530

    added on click logic

--- src/components/SuggestionsList.vue
+++ src/components/SuggestionsList.vue
@@ -14,6 +14,7 @@
           v-for="(option, index) in options"
           :key="index"
           class="select-autocomplete__option"
+          @click="openLink(option)"
         >
           {{ option }}
         </li>
@@ -47,6 +48,10 @@ export default {
       setTimeout(() => {
         this.showDropdown = false;
       }, 200);
+    },
+    openLink(option) {
+      const url = `https://www.google.com/search?q=${encodeURIComponent(option)}`;
+      window.open(url, '_blank');
     },
   },
 };
